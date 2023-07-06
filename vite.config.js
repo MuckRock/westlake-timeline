d3da6803d0b3ad27e56ec296beb60fc44c097515
Chris Amico <eyeseast@gmail.com>
2023-07-06 16:09:02 -0400

    base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/westlake-timeline/",
 	plugins: [svelte()],
 	build: {
 		rollupOptions: {
